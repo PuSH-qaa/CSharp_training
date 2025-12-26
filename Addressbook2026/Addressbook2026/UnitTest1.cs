aa7153f29a1570defb2901414056ca6bcd421caa
PuSH-qaa <fedoseev_28@mail.ru>
2025-12-25 23:22:28 -0600

    Solution and project were created

---
+++ Addressbook2026/Addressbook2026/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Addressbook2026
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //test
+        }
+    }
+}
