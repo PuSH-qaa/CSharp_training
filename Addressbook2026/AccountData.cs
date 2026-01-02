1760a4d8114bb8bcfec9fdeb2f21b504f2d4ec21
PuSH-qaa <fedoseev_28@mail.ru>
2026-01-01 19:59:49 -0600

    Some code for groups has been extracted into separate methods

---
+++ Addressbook2026/AccountData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Addressbook2026
+{
+    internal class AccountData
+    {
+        public AccountData(string username, string password) 
+        {
+            UserName = username;
+            Password = password;
+        }
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Addressbook2026/GroupData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Addressbook2026
+{
+    internal class GroupData
+    {
+        public GroupData(string name)
+        {
+            GroupName = name;
+        }
+
+        public GroupData(string name, string header, string footer)
+        {
+            GroupName = name;
+            GroupHeader = header;
+            GroupFooter = footer;
+        }
+
+        public string GroupName { get; set; }
+        public string GroupHeader { get; set; }
+        public string GroupFooter { get; set; }
+
+    }
+}
